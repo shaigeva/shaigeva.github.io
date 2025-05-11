625b999d8336646e6cdea94bf01ceab74628615c
Shai Geva <6005026+shaigeva@users.noreply.github.com>
2025-05-11 18:36:07 +0300

    docs

--- shai_site/tools/build_site.sh
+++ shai_site/tools/build_site.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env zsh
 set -x
 
+# INSTRUCTIONS:
+# Run from the shai_site directory.
+
 hugo  && (rm -rf ../docs || 1) && cp -r public ../docs && cp ../CNAME ../docs/
--- shai_site/tools/run_local_watch.sh
+++ shai_site/tools/run_local_watch.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env zsh
 set -x
 
+# INSTRUCTIONS:
+# Run from the shai_site directory.
+
 hugo server --buildDrafts
